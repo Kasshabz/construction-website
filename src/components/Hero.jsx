f75c16f750fc214e15a210de50079adc1686e4a1
Kasshabz <142917268+Kasshabz@users.noreply.github.com>
2024-11-19 20:24:00 +0000

    jpg pics

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import QuoteButton from "./QuoteButton";
-import teamPic from "../assets/team-pic.jpg";
+import teamPic from "../assets/team-pic.JPG";
 
 export default function Hero() {
   return (
