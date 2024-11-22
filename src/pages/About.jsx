34f70da4834a992180f231a0c886052e51f7359e
Kasshabz <142917268+Kasshabz@users.noreply.github.com>
2024-11-22 17:17:20 +0000

    hero image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
           {" "}
           {/* Set image height to 650px, no margin-bottom */}
           <img
-            src="src/assets/hero-image.jpg" // Replace with your image path
+            src="src/assets/hero-image.JPG" // Replace with your image path
             alt="Armstrong Buildings Team"
             className="w-full h-auto max-h-[650px] object-cover rounded-lg shadow-lg"
           />
